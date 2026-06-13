b267fb083a7c179387065c6acb62f60b8bdbf7ad
dionesankh <44867819+dionesankh@users.noreply.github.com>
2026-06-12 21:25:40 -0300

    Add files via upload

--- controle.js
+++ controle.js
@@ -658,29 +658,8 @@ function ativarAlarme(){
 }
 
 function tocarAlarme(){
-
-    // Tentar criar som com Web Audio API
-    try {
-        const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-        const oscillator = audioContext.createOscillator();
-        const gainNode = audioContext.createGain();
-        
-        oscillator.connect(gainNode);
-        gainNode.connect(audioContext.destination);
-        
-        oscillator.frequency.value = 800; // Frequência em Hz
-        oscillator.type = 'sine'; // Tipo de onda
-        
-        gainNode.gain.setValueAtTime(0.3, audioContext.currentTime);
-        gainNode.gain.exponentialRampToValueAtTime(0.01, audioContext.currentTime + 0.5);
-        
-        oscillator.start(audioContext.currentTime);
-        oscillator.stop(audioContext.currentTime + 0.5);
-        
-    } catch(e){
-        console.log("Áudio não suportado");
-    }
-
+    const audio = new Audio('alarme.mp3');
+    audio.play();
 }
 
 function abrirTelao(){
